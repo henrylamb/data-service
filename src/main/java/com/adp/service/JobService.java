8ccfe2d46e922c2747a0b9c32680c9f0d6b0d41e
Eva Posteraro <eva.posteraro@adp.com>
2024-10-18 15:41:45 +0200

    first commit

--- src/main/java/com/adp/service/JobService.java
+++ src/main/java/com/adp/service/JobService.java
@@ -7,31 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.adp.domain.Customer;
-import com.adp.repository.CustomerRepository;
+import com.adp.domain.Job;
+import com.adp.repository.JobRepository;
 
 @Service
 public class JobService{
-    @Autowired CustomerRepository repo;
+    @Autowired JobRepository repo;
 
-    public Iterable<Customer> getAll(){
+    public Iterable<Job> getAll(){
         return repo.findAll();
     }
 
-    public Optional<Customer> getCustomer(long id) {
+    public Optional<Job> getJob(long id) {
         return repo.findById(id);
     }
 
-    public URI saveCustomer(Customer customer) {
-        customer = repo.save(customer);
+    public URI saveJob(Job job) {
+        job = repo.save(job);
         return ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(customer.getId())
+                .buildAndExpand(job.getId())
                 .toUri();
     }
 
-    public void delete(Customer customer){
-        repo.delete(customer);
+    public void delete(Job job){
+        repo.delete(job);
     }
 }
