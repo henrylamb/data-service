035f81df1bfcdadbc9bac5b779ceb4b2470424a4
nicolap16 <66072944+nicolap16@users.noreply.github.com>
2024-10-18 14:06:59 +0100

    Removed an import

--- src/main/java/com/adp/service/JobService.java
+++ src/main/java/com/adp/service/JobService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.adp.domain.Job;
-import com.adp.repository.CustomerRepository;
 
 @Service
 public class JobService{
