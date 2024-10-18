be3d85cbda8473604388ce615ccb1016de35288c
nicolap16 <66072944+nicolap16@users.noreply.github.com>
2024-10-18 11:05:18 +0100

    Created schema for job and application tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,43 @@
--- ----------------------------------------------
--- DDL Statements for tables
--- ----------------------------------------------
 
-CREATE TABLE "CUSTOMERS" ("ID" INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "CUSTOMER_NAME" VARCHAR(255), "PASSWORD" VARCHAR(255),EMAIL VARCHAR(255));
+-- Create JOB table
+CREATE TABLE job (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT DEFAULT NULL,
+    department VARCHAR(25) DEFAULT NULL,
+    listing_title VARCHAR(100) DEFAULT NULL,
+    date_listed TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_closed TIMESTAMP DEFAULT NULL,
+    job_title VARCHAR(45) DEFAULT NULL,
+    job_description TEXT,
+    additional_information TEXT,
+    listing_status VARCHAR(25) DEFAULT NULL,
+    experience_level VARCHAR(100) DEFAULT NULL,
+    model_resume TEXT,
+    model_cover_letter TEXT,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 
 
--- ----------------------------------------------
--- DDL Statements for keys
--- ----------------------------------------------
+-- Create an Application table
+CREATE TABLE `application` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `job_id` int DEFAULT NULL,
+  `date_applied` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `cover_letter` text,
+  `custom_resume` text,
+  `application_status` varchar(45) DEFAULT NULL,
+  `years_of_experience` int,
+  `match_job_description_score` int,
+  `past_experience_score` int,
+  `motivation_score` int,
+  `acedemic_achievement_score` int,
+  `pedigree_score` int,
+  `trajectory_score` int,
+  `extenuating_circumstances_score` int,
+  `average_score` text,
+  `review` text,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_application_job` FOREIGN KEY (`job_id`) REFERENCES `job`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- primary/unique
-ALTER TABLE "CUSTOMERS" ADD CONSTRAINT "SQL120325130144011" PRIMARY KEY ("ID");
+-- CONSTRAINT: just a label that makes it easier to identify this foreign key relationship, while the foreign key itself (job_id) defines the actual link between an application and a specific job.
