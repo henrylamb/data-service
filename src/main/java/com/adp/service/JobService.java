87d52f268187e9ddb5d5f87a21fb2d9c48ddbf93
nicolap16 <66072944+nicolap16@users.noreply.github.com>
2024-10-18 16:00:28 +0100

    Added JobControllerTest.java

--- src/main/java/com/adp/service/JobService.java
+++ src/main/java/com/adp/service/JobService.java
@@ -24,6 +24,7 @@ public class JobService{
     }
 
     public URI saveJob(Job job) {
+        // TODO: validate job object
         job = repo.save(job);
         return ServletUriComponentsBuilder
                 .fromCurrentRequest()
