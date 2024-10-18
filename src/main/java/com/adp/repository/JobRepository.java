b24d06c973200f5ad8fe8fffa51e489cced49214
latoskasia <101577342+latoskasia@users.noreply.github.com>
2024-10-18 14:34:21 +0200

    JobRepository done

--- src/main/java/com/adp/repository/JobRepository.java
+++ src/main/java/com/adp/repository/JobRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.adp.domain.Job;
 
-public interface JobRepository extends CrudRepository<Customer, Long>{
+public interface JobRepository extends CrudRepository<Job, Long>{
 }
