4b3f9e79a5bc8e6352800f27a358f70868545a6e
albe-zheng <alberto.zheng@mail.polimi.it>
2024-10-21 17:34:55 +0200

    small fix

--- src/main/java/com/adp/domain/Application.java
+++ src/main/java/com/adp/domain/Application.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class Application {
 
     private String candidateEmail;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "job_id", referencedColumnName = "id", nullable = false)
     private Job job; 
 
