e24cc31841b5e2796f127a670f9cfaa0051c1c6a
nicolap16 <66072944+nicolap16@users.noreply.github.com>
2024-10-22 12:03:19 +0100

    Cookie decoder class pre-lunch

--- src/main/java/com/adp/util/CookieDecoder.java
+++ src/main/java/com/adp/util/CookieDecoder.java
@@ -1,44 +1,41 @@
-package com.adp.util;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.JWT;
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CookieDecoder {
 
     private final Algorithm algorithm;
 
-    public CookieDecoder(String secret) {
-        this.algorithm = Algorithm.HMAC256(secret);
+    public CookieDecoder(RSAPublicKey publicKey) {
+        this.algorithm = Algorithm.RSA256(publicKey, null); // Use the public key for verification
     }
 
     public Map<String, String> decodeCookie(HttpServletRequest request) throws JWTVerificationException {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("jwt")) {
+                if (cookie.getName().equals("user")) {
                     String token = cookie.getValue();
-                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    JWTVerifier verifier = JWT.require(algorithm).build(); // This will throw an exception if the token is invalid
                     DecodedJWT decodedJWT = verifier.verify(token);
 
                     // Extract claims from the token
                     Map<String, String> claims = new HashMap<>();
-                    claims.put("email", decodedJWT.getClaim("email").asString());
                     claims.put("role", decodedJWT.getClaim("role").asString());
-                    claims.put("department", decodedJWT.getClaim("department").asString());
+                    claims.put("userId", decodedJWT.getClaim("userId").asString());
 
                     return claims;
                 }
             }
         }
-        throw new JWTVerificationException("No valid JWT token found");
+        return null;
     }
 }
