4f24037aa19e2b81b9230649259415b34b62f219
latoskasia <101577342+latoskasia@users.noreply.github.com>
2024-10-23 12:30:48 +0200

    Endpoints tested for all the roles - fixing bugs

--- src/main/java/com/adp/controller/ApplicationController.java
+++ src/main/java/com/adp/controller/ApplicationController.java
@@ -24,7 +24,7 @@ public class ApplicationController {
     @Autowired
     JobService jobService;
 
-    @PreAuthorize("hasRole('CANDIDATES')")
+    @PreAuthorize("hasRole('CANDIDATE')")
     @GetMapping("/{id}")
     public ResponseEntity<?> getApplicationById(@PathVariable("id") long id) {
         Optional<Application> optApplication = applicationService.getApplication(id);
@@ -34,7 +34,7 @@ public class ApplicationController {
         return ResponseEntity.ok(optApplication.get());
     }
 
-    @PreAuthorize("hasRole('MANAGERS')")
+    @PreAuthorize("hasRole('MANAGER')")
     @GetMapping("/{id}/statistics")
     public ResponseEntity<Application> getApplicationStatistics(@PathVariable("id") long id) {
         Optional<Application> optApplication = applicationService.getApplication(id);
@@ -49,7 +49,7 @@ public class ApplicationController {
         return ResponseEntity.ok(application);
     }
 
-    @PreAuthorize("hasRole('MANAGERS')")
+    @PreAuthorize("hasRole('MANAGER')")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteApplication(@PathVariable("id") long id) {
         // delete application
@@ -58,7 +58,7 @@ public class ApplicationController {
         return ResponseEntity.noContent().build();
     }
 
-    @PreAuthorize("hasRole('CANDIDATES')")
+    @PreAuthorize("hasRole('CANDIDATE')")
     @PutMapping("/{id}")
     public ResponseEntity<Application> putApplication(@PathVariable("id") long id, @RequestBody ApplicationRequest applicationReq) {
         System.out.println(applicationReq);
@@ -84,7 +84,7 @@ public class ApplicationController {
         return ResponseEntity.ok(application);
     }
 
-    @PreAuthorize("hasRole('MANAGERS')")
+    @PreAuthorize("hasRole('MANAGER')")
     @PutMapping("/manager/{id}")
     public ResponseEntity<Application> putApplicationManager(@PathVariable("id") long id, @RequestBody ApplicationRequest applicationReq) {
         Optional<Job> jobOptional = jobService.getJob(applicationReq.getJobId());
@@ -115,7 +115,7 @@ public class ApplicationController {
         return ResponseEntity.ok(application);
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_CANDIDATE','ROLE_MANAGER','ROLE_ADMIN')") //TODO check if all have permission
+    @PreAuthorize("hasAnyRole('ROLE_CANDIDATE','ROLE_MANAGER','ROLE_ADMIN')") //TODO  what's going on
     @GetMapping("/page")
     public ResponseEntity<Page<Application>> getApplications(@RequestParam("page") int page, @RequestParam("items") int items) {
 
@@ -123,7 +123,7 @@ public class ApplicationController {
         return ResponseEntity.ok(applications);
     }
     
-    @PreAuthorize("hasRole('CANDIDATES')")
+    @PreAuthorize("hasRole('CANDIDATE')")
     @PostMapping
     public ResponseEntity<Application> addApplication(@RequestBody ApplicationRequest applicationReq) {
         System.out.println(applicationReq);
